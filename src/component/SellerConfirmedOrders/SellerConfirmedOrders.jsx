8a85c2fee6111b10c4b60701f38e19320ea4b923
alaaaziz2020 <alaaaziz017@gmail.com>
2025-12-14 09:11:54 +0200

    first commit

--- src/component/SellerConfirmedOrders/SellerConfirmedOrders.jsx
+++ src/component/SellerConfirmedOrders/SellerConfirmedOrders.jsx
@@ -1,7 +1,8 @@
-import React, { useState } from "react";
+import React from "react";
 import "../SellerConfirmedOrders/SellerConfirmedOrders.css";
 import { useNavigate, useLocation } from "react-router-dom";
-import wintershirt from "../../assets/winter-shirt.jpg";useState
+import wintershirt from "../../assets/winter-shirt.jpg";
+
 export default function SellerConfirmedOrders() {
   const navigate = useNavigate();
   const location = useLocation();
@@ -22,27 +23,33 @@ export default function SellerConfirmedOrders() {
       <div className="container">
         <div className="row">
 
-          {/* ✅ التابات */}
-          <div className="col-lg-3  d-lg-flex justify-content-start tabssection">
+          {/* التابات */}
+          <div className="col-lg-3 d-lg-flex justify-content-start tabssection">
             <div className="orders-right-tabs">
 
               <button
-                className={`cright-tab cright-btn ${location.pathname === "/confirmedorders" ? "active" : ""}`}
+                className={`cright-tab cright-btn ${
+                  location.pathname === "/seller/sellerconfirmedorders" ? "active" : ""
+                }`}
                 onClick={() => navigate("/seller/sellerconfirmedorders")}
               >
                 الطلبات الحالية
               </button>
 
               <button
-                className={`cright-tab cright-btn2 ${location.pathname === "/shippedorders" ? "active" : ""}`}
+                className={`cright-tab cright-btn2 ${
+                  location.pathname === "/seller/sellershippedorders" ? "active" : ""
+                }`}
                 onClick={() => navigate("/seller/sellershippedorders")}
               >
                 الطلبات المكتملة
               </button>
 
               <button
-                className={`cright-tab cright-btn3${location.pathname === "/canceledorders" ? "active" : ""}`}
-                onClick={() => navigate("seller/sellercanceledorders")}
+                className={`cright-tab cright-btn3 ${
+                  location.pathname === "/seller/sellercanceledorders" ? "active" : ""
+                }`}
+                onClick={() => navigate("/seller/sellercanceledorders")}
               >
                 الطلبات الملغية
               </button>
@@ -50,12 +57,12 @@ export default function SellerConfirmedOrders() {
             </div>
           </div>
 
-          {/* ✅ الكروت */}
+          {/* الكروت */}
           <div className="col-lg-9 col-12 justify-content-start">
             <div className="cards-wrapper mt-5">
               {orders.map((order, i) => (
                 <div key={i} className="corder-mobile-card">
-                  <img src={order.img} alt="" className="corder-mobile-img " />
+                  <img src={order.img} alt="" className="corder-mobile-img" />
                   <div className="order-mobile-content">
                     <h3 className="corder-mobile-title">{order.title}</h3>
                     <p className="corder-mobile-desc">{order.desc}</p>
